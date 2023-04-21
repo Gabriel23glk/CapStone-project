63e1d23e147bd8a0cf40d35a828cc002ae5322e5
Gabe Arnold <gabearnold237@gmail.com>
2023-04-20 23:04:34 -0600

    final publish to github

--- src/capStone_Motor_01.cpp
+++ src/capStone_Motor_01.cpp
@@ -14,11 +14,15 @@
 #include "Adafruit_SSD1306.h"
 #include "colors.h"
 #include "cartoon_Cat.h"
+#include <Adafruit_MQTT.h>
+#include "Adafruit_MQTT/Adafruit_MQTT_SPARK.h"
+#include "Adafruit_MQTT/Adafruit_MQTT.h"
+#include "credentials.h"
 void setup();
 void loop();
 void onDataReceived(const uint8_t* data, size_t len,const BlePeerDevice& peer, void* context);
 void pixelFill(int startPix,int endPix,int color);
-#line 11 "c:/Users/gabea/Documents/IoT/capStone_Motor_01/src/capStone_Motor_01.ino"
+#line 15 "c:/Users/gabea/Documents/IoT/capStone_Motor_01/src/capStone_Motor_01.ino"
 const size_t UART_TX_BUF_SIZE = 4;
 uint8_t txBuf[UART_TX_BUF_SIZE];
 uint8_t h;
@@ -49,26 +53,32 @@ int color2=blue;
 int pixelCount;
 const int PIXELPIN=A3;
 const int PIXELNUMBER=15;
+int distanceVal;
+unsigned int lastTime;
 Adafruit_NeoPixel pixel(PIXELNUMBER,PIXELPIN,WS2812B);
 Adafruit_SSD1306 display(OLED_RESET);
-SYSTEM_MODE(SEMI_AUTOMATIC);
-
+TCPClient TheClient;
+Adafruit_MQTT_SPARK mqtt(&TheClient,AIO_SERVER,AIO_SERVERPORT,AIO_USERNAME,AIO_KEY);
+Adafruit_MQTT_Publish rssiFeed=Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/rssi");
+void MQTT_connect();
+bool MQTT_ping();
 
 void setup() {
- Serial.begin(9600);
-  waitFor(Serial.isConnected, 15000);
-    BLE.on();
-    BLE.addCharacteristic(txCharacteristic);
-    BLE.addCharacteristic(rxCharacteristic);
-    data.appendServiceUUID(serviceUuid);
-    BLE.advertise(&data);
-    Serial.printf("Argon BLE Address:%s\n",BLE.address().toString().c_str());
-display.begin (SSD1306_SWITCHCAPVCC, 0x3c);
+   WiFi.on();
+   WiFi.connect();
+   Serial.begin(9600);
+   waitFor(Serial.isConnected, 15000);
+   BLE.on();
+   BLE.addCharacteristic(txCharacteristic);
+   BLE.addCharacteristic(rxCharacteristic);
+   data.appendServiceUUID(serviceUuid);
+   BLE.advertise(&data);
+   Serial.printf("Argon BLE Address:%s\n",BLE.address().toString().c_str());
+  display.begin (SSD1306_SWITCHCAPVCC, 0x3c);
   display.display();
   display.clearDisplay();
   display.drawBitmap(16,20, myBitmap,112, 44, 1);
   display.display();
-  // delay(1000);
   display.begin(SSD1306_SWITCHCAPVCC, 0x3c);
   display.setTextSize(2);
   display.setTextColor(BLACK,WHITE);
@@ -93,6 +103,7 @@ void loop() {
         }
         if (mac[i][0]==0xFA &&mac[i][1]==0xDA){
             Serial.printf("FOUND=%02X:%02X:%02X:%02X:%02X:%02X,RSSI=%i\n",mac[i][0],mac[i][1],mac[i][2],mac[i][3],mac[i][4],mac[i][5],rssi[i]);
+            distanceVal=rssi[i];
             if(rssi[i]>=-55){
             digitalWrite(D10,HIGH);
           
@@ -100,15 +111,17 @@ void loop() {
             if(rssi[i]<=-60){
               digitalWrite(D10,LOW);
             }
-        }
-        // Serial.printf("MAC address = %02X:%02X:%02X:%02X:%02X:%02X. RSSI = %i\n",mac[i][0],mac[i][1],mac[i][2],mac[i][3],mac[i][4],mac[i][5],rssi[i]);
+             if((millis()-lastTime>2000)){
+    if(mqtt.Update()){
+         rssiFeed.publish(rssi[i]);
+        Serial.printf("Publishing %i \n",rssi);
+         lastTime=millis(); 
     }
-  delay(5000);
-  
+   }
 
+        }
+    }
   }
-
-
 
 void onDataReceived(const uint8_t* data, size_t len,const BlePeerDevice& peer, void* context) {
     uint8_t h;
@@ -132,9 +145,8 @@ if (OnorOff==1){
 if(OnorOff==0){
   digitalWrite(D10,LOW);
   ("turn off button%i\n",OnorOff);
-   pixelFill(startPixel,endPixel,color1);
+  pixel.clear();     
   pixelFill(startPixel,endPixel,color2);
-  pixel.clear();
 }
 }
 void pixelFill(int startPix,int endPix,int color){
@@ -142,4 +154,38 @@ void pixelFill(int startPix,int endPix,int color){
       pixel.setPixelColor(pixelCount,color1);
       pixel.setPixelColor(pixelCount+1,color2); 
     }
+}
+void MQTT_connect(){
+  int8_t ret;
+ 
+  // Return if already connected.
+  if (mqtt.connected()) {
+    return;
+  }
+ 
+  Serial.print("Connecting to MQTT... ");
+ 
+  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+       Serial.printf("Error Code %s\n",mqtt.connectErrorString(ret));
+       Serial.printf("Retrying MQTT connection in 5 seconds...\n");
+       mqtt.disconnect();
+       delay(5000);  // wait 5 seconds and try again
+  }
+  Serial.printf("MQTT Connected!\n");
+}
+
+bool MQTT_ping(){
+  static unsigned int last;
+  bool pingStatus;
+
+  if ((millis()-last)>120000) {
+      Serial.printf("Pinging MQTT \n");
+      pingStatus = mqtt.ping();
+      if(!pingStatus) {
+        Serial.printf("Disconnecting \n");
+        mqtt.disconnect();
+      }
+      last = millis();
+  }
+  return pingStatus;
 }
